5fceb75ac84de3d53aab08aea710aa8322ce4d25
MR-GREEN1337 <islam.hachimi2003@gmail.com>
2025-04-02 01:16:05 +0200

    Improved Web landing page

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
 
+app = FastAPI(name="ustadh backend", description="Ustadh backend", version="0.1.0")
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
